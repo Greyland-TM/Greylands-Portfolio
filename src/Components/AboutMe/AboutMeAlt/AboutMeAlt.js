6c1b20ee09461431d1eacf1558f21ca1cbf68028
Greyland-TM <greyland.official@gmail.com>
2020-12-29 19:59:41 -0800

    try again

--- src/Components/AboutMe/AboutMeAlt/AboutMeAlt.js
+++ src/Components/AboutMe/AboutMeAlt/AboutMeAlt.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../AboutMeAlt/AboutMeAlt.css';
+import AltImg from '.././../../Media/Me.jpg';
 
 const AboutMeAlt = () => (
     <Container className='Alt-Container'>
         <Row>
             <Col>
+                <img className='About-Me-Img'
+                    src={AltImg}
+                    alt='Profile'
+                ></img>
+            </Col>
+            <Col>
                 <h2>Continued</h2>
                 <p>More about me</p>
             </Col>
-            <Col></Col>
         </Row>
     </Container>
 );
